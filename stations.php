c629937fbb2e9f053b405d668745de408bc5693c
officerNordberg <drew.michael.thompson@gmail.com>
2013-03-20 02:17:57 -0500

    removing commented out obsolete line

--- stations.php
+++ stations.php
@@ -1,12 +1,11 @@
 <?php
-		$stationListText = file_get_contents("stations");
-		$stations = explode("|", $stationListText);
-		$json = array();
+	$stationListText = file_get_contents("stations");
+	$stations = explode("|", $stationListText);
+	$json = array();
        	foreach($stations as $stationText){
-    			$station = explode("=", $stationText);
-    			//$stationArray = array($station[0],$station[1]);
-    			array_push($json, $station[1]);
+    		$station = explode("=", $stationText);
+    		array_push($json, $station[1]);
        	}
-		echo json_encode($json, JSON_FORCE_OBJECT);
-		die();
+	echo json_encode($json, JSON_FORCE_OBJECT);
+	die();
 ?>
