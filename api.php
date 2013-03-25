eaf0b4500283244ff6cb879c256926318a99a28c
Drew Thompson <drew.michael.thompson@gmail.com>
2013-03-24 20:52:07 -0500

    removing dead code

--- api.php
+++ api.php
@@ -9,12 +9,4 @@ if (isset($_POST['control']))
 	file_put_contents("ctl", "$control");
 	if ($control == "n") file_put_contents("msg", "Skipped");
 } 
-
-//if (file_exists("msg"))
-//{
-//	$msg = file_get_contents("msg");
-//	unlink("msg");
-//	return "<h1 class=msg>$msg</h1>";
-//}
-//	if ($love==1) $return .= "<img src=imgs/love.png class=love width=20 />";
 ?>
